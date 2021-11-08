aed3d06c37a1866c6a5e8048314c0995a1435f9a
marcolimone <92522674+marcolimone@users.noreply.github.com>
2021-11-08 15:40:01 +0100

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -32,10 +32,29 @@ def turn(speed, seconds):
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
 
+def find_silver_token_near():    
+    """
+    Function to find a silver token at a very low distance
+
+    Returns:
+  dist (float): distance of the closest silver token (-1 if no silver token is detected)
+  rot_y (float): angle between the robot and the silver token (-1 if no silver token is detected)
+    """
+    dist = 1
+
+    for token in R.see():
+        if token.dist < dist and token.info.marker_type is MARKER_TOKEN_SILVER and -100 < token.rot_y < +100:
+            dist=token.dist
+            rot_y=token.rot_y
+    if dist == 1:
+        return -1, -1
+    else:
+        return dist, rot_y
+ 
 
 def find_silver_token():    
     """
-    Function to find the closest silver token
+    Function to find the closest silver token in a cone in front of the robot
 
     Returns:
   dist (float): distance of the closest silver token (-1 if no silver token is detected)
@@ -79,7 +98,7 @@ def find_golden_token_left():
     Returns:
   dist (float): distance of the closest golden token (-1 if no golden token is detected)
   rot_y (float): angle between the robot and the golden token (-1 if no golden token is detected)
-    """
+    """ 
     dist=1000
     for token in R.see():
         if token.dist < dist and token.info.marker_type is MARKER_TOKEN_GOLD and -100 < token.rot_y < -75: 
@@ -140,9 +159,34 @@ def main():
    d_silver , rot_y_silver = find_silver_token()
    d_golden , rot_y_golden = find_golden_token()
    dist_golden , rot_y_golden_intra = find_intra_golden_token (rot_y_silver)
+   d_silver_near , rot_y_silver_near = find_silver_token_near()
+   
+   if d_silver_near != -1 :                            # """check done to see any very close silver tokens not seen"""
+      
+      if d_silver_near < d_th_silver:                  # """if the robot is close enough to the token, we try grab it"""
+         print("Found near me!")
+         if R.grab():                                  # """if the robot grab the token, the robot move forward and on the right, it release the token, and it go back to the initial position"""            
+            print("Gotcha!")
+	    turn(30, 2)
+	    print("Put it behind me")
+	    R.release()
+	    print("Realise it")
+            turn(-30,2)  
+         
+                
+         
+      elif -a_th<= rot_y_silver_near <= a_th:                        #"""if the robot is well aligned with the token, we go forward"""
+         print("Oh, this way")
+         drive(50, 0.02)
+      elif rot_y_silver_near < -a_th:                                # """if the robot is not well aligned with the token, we move it on the left or on the right"""
+         print("Left a bit")
+         turn(-2, 0.5)
+      elif rot_y_silver_near > a_th:
+         print("Right a bit")
+         turn(+2, 0.5)
     
       
-   if d_golden<d_th_golden  :                                       #"""control of the distance beetween robot and closest detected golden token"""
+   elif d_golden<d_th_golden  :                                       #"""control of the distance beetween robot and closest detected golden token"""
       d_golden_right, rot_y_golden_r = find_golden_token_right()
       d_golden_left , rot_y_golden_l = find_golden_token_left()
       if d_golden_right > d_golden_left :                           # """control for the direction in which the robot has to turn"""
@@ -164,17 +208,7 @@ def main():
        print("nothing found")
        drive(50,0.02)  
     
-   elif d_silver < d_th_silver:                        # """if the robot is close enough to the token, we try grab it"""
-       print("Found it!")
-       if R.grab():                                    # """if the robot grab the token, the robot move forward and on the right, it release the token, and it go back to the initial""" position"            
-           print("Gotcha!")
-	   turn(30, 2)
-	   print("Put it behind me")
-	   R.release()
-	   print("Realise it")
-           turn(-30,2)  
-       else:
-           drive(50,0.02)
+   
                 
    
    elif -a_th<= rot_y_silver <= a_th:                        #"""if the robot is well aligned with the token, we go forward"""
