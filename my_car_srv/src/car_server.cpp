d96764c7bed81badb381bbbacc5bc663739188f5
marcolimone <92522674+marcolimone@users.noreply.github.com>
2021-12-07 11:49:38 +0100

    Update car_server.cpp

--- my_car_srv/src/car_server.cpp
+++ my_car_srv/src/car_server.cpp
@@ -1,18 +1,18 @@
 #include "ros/ros.h"
 #include "my_car_srv/Velocity_car.h"
 
-float my_vel= 0.0;
+float my_vel= 0.0;                     //define and initialize a global variable that contains the velocity of our robot
 
 bool myvel (my_car_srv::Velocity_car::Request &req, my_car_srv::Velocity_car::Response &res){
-        if (req.velocity == 1)
+        if (req.velocity == 1)                          //request 1 add a velocity unit to our velocity
         {
           res.mult=my_vel+1.0;
         }
-        else if (req.velocity == 2)
+        else if (req.velocity == 2)                      //request 2 substract a velocity unit to our velocity
         {
           res.mult=my_vel-1.0;        
         }
-        else if (req.velocity == 3)
+        else if (req.velocity == 3)                     //request 3 give back the current velocity
         {
           res.mult=my_vel;                
         }
